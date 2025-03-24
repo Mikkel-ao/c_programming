f7ddf43b3906a2374ffefbc48881c350bdaff6d6
Mikkel-ao <mikkel.ao.dev@gmail.com>
2025-03-24 22:11:32 +0100

    notes in comments

--- chapter2/pun.c
+++ chapter2/pun.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 
+// compile + link: cc pun.c
+// Rename the executable cc -o <name> pun.c
+// GGC compiler. To compile gcc -o pun pun.c
+
 int main(void)
 {
     printf("To C, or not to C: that is the question.\n");
